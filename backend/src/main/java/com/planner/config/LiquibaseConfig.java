1edfc164185b18241b799d05e320139a67ad2df7
MrLynx93 <mateusz.ziebura93@gmail.com>
2026-03-27 00:39:28 +0100

    Add example test data

--- backend/src/main/java/com/planner/config/LiquibaseConfig.java
+++ backend/src/main/java/com/planner/config/LiquibaseConfig.java
@@ -3,6 +3,8 @@ package com.planner.config;
 import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
@@ -10,10 +12,31 @@ import javax.sql.DataSource;
 public class LiquibaseConfig {
 
     @Bean
+    @Profile("h2")
     public SpringLiquibase liquibase(DataSource dataSource) {
         SpringLiquibase liquibase = new SpringLiquibase();
         liquibase.setDataSource(dataSource);
         liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.yml");
+        liquibase.setDropFirst(true);
+        return liquibase;
+    }
+
+    @Bean
+    @Profile("postgres")
+    public SpringLiquibase liquibasePostgres(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.yml");
+        return liquibase;
+    }
+
+    @Bean
+    @Profile("h2")
+    @DependsOn("liquibase")
+    public SpringLiquibase testDataLiquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:db/changelog/db.changelog-test-data.yml");
         return liquibase;
     }
 }
